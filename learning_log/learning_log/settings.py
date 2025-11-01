071be36a92004b1671dc2b8972f192f3c5a242ee
kbulbul026-coder <kbulbul026@gmail.com>
2025-11-01 09:23:13 +0530

    working in local

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -104,14 +104,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
